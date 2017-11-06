9077fba1894c5a44e0e57c72e6e3cdd24c61436e
Sale <aleksandarjovanovic996@yahoo.com>
2017-11-06 11:54:56 +0100

    Info i cenovnik

--- resources/views/putovanje.blade.php
+++ resources/views/putovanje.blade.php
@@ -92,6 +92,34 @@
             font-family: Montserrat,sans-serif;
             margin: unset;
         }
+        .opis-naziv{
+
+            font-family: Montserrat,sans-serif;
+            line-height: 1.25em;
+            font-size: 17px;
+            color: rgba(48, 48, 48, 0.81);
+            font-weight: 700;
+        }
+        .opis-text{
+            font-family: Montserrat,sans-serif;
+            font-weight: 300;
+            font-size: 15px;
+            line-height: 25px;
+        }
+        .opis-text > div > i {
+
+            color: #fed700;
+        }
+        .fa, .fa-star{
+            color:rgba(0,0,0,0.2);
+        }
+        .checked {
+            color: orange;
+        }
+        .text-center{
+            text-align: center;
+        }
+
     </style>
     @endpush
     <header id="fh5co-header-section" class="sticky-banner">
@@ -135,14 +163,14 @@
 
     <div class="">
         <ul class="nav nav-tabs nav-bar-putovanja">
-            <li class="active"><a href="#Informacije" data-toggle="tab"><i class="glyphicon glyphicon-list-alt"></i> Informacije</a></li>
-            <li><a href="#Cenovnik" data-toggle="tab" ><i class="glyphicon glyphicon-eur"></i> Cenovnik</a></li>
+            <li ><a href="#Informacije" data-toggle="tab"><i class="glyphicon glyphicon-list-alt"></i> Informacije</a></li>
+            <li class="active"><a href="#Cenovnik" data-toggle="tab" ><i class="glyphicon glyphicon-eur"></i> Cenovnik</a></li>
             <li><a href="#PlanPuta" data-toggle="tab"><i class="fa fa-map-o"></i> Plan puta</a></li>
             <li><a href="#Smestaji" data-toggle="tab"><i class="fa fa-building-o"></i> Smestaji</a></li>
             <li><a href="#Izleti" data-toggle="tab"><i class="fa fa-tree"></i> Izleti</a></li>
         </ul>
         <div class="tab-content clearfix">
-            <div class="tab-pane active" id="Informacije">
+            <div class="tab-pane" id="Informacije">
                 <br>
                 <div class="container">
                     <div class="row">
@@ -166,9 +194,63 @@
                                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec posuere metus et tortor pulvinar venenatis. Aliquam erat volutpat. Nam ultrices semper felis, at laoreet metus laoreet a. Praesent orci orci, pretium ut tortor euismod, vehicula blandit purus. Aliquam suscipit, erat a maximus mollis, neque odio aliquam arcu, ut consequat leo nisi sit amet leo. Nam consectetur diam id libero fringilla, eu commodo arcu congue.</p>
                             </div>
                             <br>
+                            <div>
+                                <i class="glyphicon glyphicon-time"></i>
+                                10 Dana
+                            </div>
+                            <br>
+
+                            <div class="">
+                                <div class="row">
+                                    <hr>
+                                    <div class="col-md-4 opis-naziv">Destinacija</div>
+                                    <div class="col-md-3 opis-text">Grcka</div>
+                                </div>
+                                <div class="row">
+                                    <hr>
+                                    <div class="col-md-4 opis-naziv">Polazak</div>
+                                    <div class="col-md-3 opis-text">Nis</div>
+                                </div>
+                                <div class="row">
+                                    <hr>
+                                    <div class="col-md-4 opis-naziv">Vreme polaska</div>
+                                    <div class="col-md-3 opis-text">8:00</div>
+                                </div>
+                                <div class="row">
+                                    <hr>
+                                    <div class="col-md-4 opis-naziv">Cena obuhvata</div>
+                                    <div class="col-md-3 opis-text">
+                                        <div>
+                                            <i class="glyphicon glyphicon-ok"></i> Dorucak
+                                        </div>
+                                        <div>
+                                            <i class="glyphicon glyphicon-ok"></i> Rucak
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <hr>
+                                    <div class="col-md-4 opis-naziv">Cena ne obuhvata</div>
+                                    <div class="col-md-3 opis-text">
+                                        <div>
+                                            <i class="glyphicon glyphicon-remove"></i> Boravisna taksa
+                                        </div>
+                                        <div>
+                                            <i class="glyphicon glyphicon-remove"></i> Izlete
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <br>
                         </div>
                         <div class="col-md-4">
-                            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSekWQ54JyyBrggP9B_gD_BvnXNR73b8BXqMz9Gb4P6PQ_DA8w/viewform?embedded=true" width="auto" height="500" frameborder="0" marginheight="1" marginwidth="1">Loading...</iframe>
+                            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSekWQ54JyyBrggP9B_gD_BvnXNR73b8BXqMz9Gb4P6PQ_DA8w/viewform?embedded=true" width="auto" height="700" frameborder="0" marginheight="1" marginwidth="1">Loading...</iframe>
+                        </div>
+                        <div class="col-md-10">
+                            <hr>
+
                         </div>
                     </div>
 
@@ -176,8 +258,88 @@
 
 
             </div>
-            <div class="tab-pane" id="Cenovnik">
-                <h3>CEN Content's background color is the same for the tab</h3>
+            <div class="tab-pane active" id="Cenovnik">
+                <br>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h1 class="putovanje-naziv">Letovanje Grcka</h1>
+                            <div class="cenovnik">
+                                <div class="heder-cenovnika row">
+                                    <div class="col-xs-6 opis-naziv">
+                                        Smestaj
+                                    </div>
+                                    <div class="col-xs-3 opis-naziv">
+                                        Tip smestaja
+                                    </div>
+                                    <div class="col-xs-3 opis-naziv">
+                                        29.12. - 03.01.
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="content-cenovnika row">
+                                    <div class="col-xs-6 opis-text">
+                                        <div>
+                                            <i class="glyphicon glyphicon-info-sign"></i> Hotel Start 2*
+                                            <div class="rating">
+                                                <span class="fa fa-star checked"></span>
+                                                <span class="fa fa-star checked"></span>
+                                                <span class="fa fa-star "></span>
+                                                <span class="fa fa-star"></span>
+                                                <span class="fa fa-star"></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-3 opis-text">
+                                        <div>1/4 sa doručkom</div>
+                                        <div>1/3 sa doručkom</div>
+                                        <div>1/2 sa doručkom</div>
+                                    </div>
+                                    <div class="col-xs-3 opis-text">
+                                        <div class="text-center">105 <i class="glyphicon glyphicon-eur"></i></div>
+                                        <div class="text-center">115 <i class="glyphicon glyphicon-eur"></i></div>
+                                        <div class="text-center">125 <i class="glyphicon glyphicon-eur"></i></div>
+
+                                    </div>
+                                </div>
+                                <hr>
+                                <div class="content-cenovnika row">
+                                    <div class="col-xs-6 opis-text">
+                                        <div>
+                                            <i class="glyphicon glyphicon-info-sign"></i> HOTEL Q 3++*
+                                            <div class="rating">
+                                                <span class="fa fa-star checked"></span>
+                                                <span class="fa fa-star checked"></span>
+                                                <span class="fa fa-star checked"></span>
+                                                <span class="fa fa-star"></span>
+                                                <span class="fa fa-star"></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xs-3 opis-text">
+                                        <div>1/4 sa doručkom</div>
+                                        <div>1/3 sa doručkom</div>
+                                        <div>1/2 sa doručkom</div>
+                                    </div>
+                                    <div class="col-xs-3 opis-text">
+                                        <div class="text-center">105 <i class="glyphicon glyphicon-eur"></i></div>
+                                        <div class="text-center">115 <i class="glyphicon glyphicon-eur"></i></div>
+                                        <div class="text-center">125 <i class="glyphicon glyphicon-eur"></i></div>
+
+                                    </div>
+                                </div>
+                            </div>
+                            <br>
+                        </div>
+
+                        <div class="col-md-10">
+                            {{--<hr>--}}
+
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
             <div class="tab-pane" id="PlanPuta">
                 <h3>PLAN We use the class nav-pills instead of nav-tabs which automatically creates a background color for the tab</h3>
